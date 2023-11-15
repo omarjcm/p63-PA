51dc5e7abcf37201b7e9591fda3ecd34fd1288f9
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-11-15 11:14:50 -0500

    Lectura y cambio de texto con expresiones regulares.

---
+++ code/unidad_02/u2_taller_01/taller_01.py
@@ -0,0 +1,11 @@
+import os
+import re
+
+ruta = 'texto2.txt'
+print("Ruta del archivo:", ruta)
+
+with open(ruta, 'r') as reader:
+    for line in reader.readlines():
+        ipsum = re.compile(r'\bipsum\b')
+        ten_cuidado = ipsum.sub('Ten cuidado', line)
+        print(ten_cuidado)
