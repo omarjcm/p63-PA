84608f7f76d9828b6de3645f24b4342ae24e3a69
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2023-11-15 10:26:21 -0500

    Talleres en clases.

--- code/unidad_02/u2_taller_01/app2.py
+++ code/unidad_02/u2_taller_01/app2.py
@@ -8,11 +8,22 @@ como un débil cristal.
 ¡todo sucederá! Podrá la muerte 
 cubrirme con su fúnebre crespón; 
 Pero jamás en mí podrá apagarse 
-la llama de tu amor.
-"""
+la llama de tu amor."""
 
 # dividir donde no se encuentre un caracter alfanumerico
 patron = re.compile(r'\W+')
 palabras = patron.split( becquer )
 
 print(palabras[:10])
+#print(palabras[3:10])
+#print(palabras[3:])
+
+palabras = re.split('\n', becquer)
+print(palabras)
+
+palabras = patron.split(becquer, 5)
+print(palabras)
+
+podra = re.compile(r'\b(P|p)odrá\b')
+puede = podra.sub('Puede', becquer)
+print(puede)
