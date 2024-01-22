56b7e15453c0be512fe46784150100903de514a4
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2024-01-22 11:24:35 -0500

    estructura del proyecto para el uso de la base de datos.

---
+++ code/unidad_04/u4_taller_02/src/datos/Archivo.java
@@ -0,0 +1,54 @@
+package datos;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Archivo {
+
+    private Conexion conexion;
+    private String nombreArchivo;
+    private InputStream archivo;
+
+    public Archivo() {
+        this.nombreArchivo = "bd.properties";
+        this.conexion = new Conexion();
+    }
+
+    public void setConexion(Conexion conexion) {
+        this.conexion = conexion;
+    }
+    public Conexion getConexion() {
+        return conexion;
+    }
+
+    public void leerArchivo() {
+        try {
+            Properties prop = new Properties();
+            String ruta = System.getProperty("user.dir");
+            this.archivo = new FileInputStream(ruta+"\\resources\\"+this.nombreArchivo);
+            if (this.archivo != null) {
+                prop.load(this.archivo);
+
+                this.conexion.setDriver( prop.getProperty("bd.driver") );
+                this.conexion.setDriverClass( prop.getProperty("bd.driverclass") );
+                this.conexion.setHost( prop.getProperty("bd.host") );
+                this.conexion.setPuerto( prop.getProperty("bd.puerto") );
+                this.conexion.setUsuario( prop.getProperty("bd.usuario") );
+                this.conexion.setClave( prop.getProperty("bd.clave") );
+                this.conexion.setNombre( prop.getProperty("bd.nombrebd") );
+
+                this.archivo.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("[error] - " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Archivo archivo = new Archivo();
+        archivo.leerArchivo();
+    }
+}
---
+++ code/unidad_04/u4_taller_02/src/datos/BD.java
@@ -0,0 +1,45 @@
+package datos;
+
+import java.sql.*;
+
+public class BD {
+
+    private Connection conexion;
+    private Archivo datos;
+
+    public BD() {
+        this.datos = new Archivo();
+        this.datos.leerArchivo();
+    }
+    
+    public void conectar() {
+        try {
+            Class.forName(this.datos.getConexion().getDriverClass());
+            this.conexion = DriverManager.getConnection(this.datos.getConexion().urlConexion(),
+                                                        this.datos.getConexion().getUsuario(),
+                                                        this.datos.getConexion().getClave());
+            System.out.println("[db] - Conexion exitosa.");
+        } catch (ClassNotFoundException e) {
+            System.out.println("[error] - " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("[error] - " + e.getMessage());
+        }
+    }
+
+    public void cerrar() {
+        try {
+            if (this.conexion != null) {
+                this.conexion.close();
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println("[error] - " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        BD conexion = new BD();
+        conexion.conectar();
+        conexion.cerrar();
+    }
+}
